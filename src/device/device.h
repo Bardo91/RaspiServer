0016d6eace8a2dededc63c191b33ddc608de1f61
technik <cjtech90@gmail.com>
2014-08-27 13:43:37 +0200

    Device interface

---
+++ src/device/device.h
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project: DMC Server
+// Date:	2014/Aug/27
+// Author:	Carmelo J. Fdez-Agüera Tortosa
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef _DMCSERVER_DEVICE_DEVICE_H_
+#define _DMCSERVER_DEVICE_DEVICE_H_
+
+namespace dmc {
+
+	class Device;
+
+	class Device {
+	public:
+		unsigned id() const { return mId; }
+
+	private:
+		unsigned	mId;
+	};
+
+}	// namespace dmc
+
+#endif // _DMCSERVER_DEVICE_DEVICE_H_
