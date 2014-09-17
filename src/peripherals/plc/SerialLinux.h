c91fff45092df3b2b7ba9e5ae549b2020debc759
technik <cjtech90@gmail.com>
2014-09-17 17:55:49 +0200

    Fixed a bad copy-paste (pi/6)

--- src/peripherals/plc/SerialLinux.h
+++ src/peripherals/plc/SerialLinux.h
@@ -27,54 +27,12 @@ namespace dmc {
 		SerialLinux			(const char* _port, unsigned _baudRate);
 
 	private:
-		void openPort		(const char* _port);
-		void setBaudRate	(unsigned _baudRate);
-		
-	private:
-		int		mFileDesc;
-	};////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// Project: DMC Server
-// Date:	2014/Aug/25
-// Author:	Carmelo J. Fdez-Agüera Tortosa
-//
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Simple serial port communications
-#ifndef _DMCSERVER_PERIPHERALS_PLC_SERIALLINUX_H_
-#define _DMCSERVER_PERIPHERALS_PLC_SERIALLINUX_H_
-
-#ifdef __linux__
-
-#include <cstdint>
-
-namespace dmc {
-
-	class SerialLinux {
-	public:
-		unsigned	write	(const void* _src, unsigned _nBytes);
-		bool		write	(uint8_t);
-
-		unsigned	read (void * _dst, unsigned _nBytes); // Returns the amount of bytes read
-		uint8_t		read(); // Reads one byte
-
-	protected:
-		SerialLinux			(const char* _port, unsigned _baudRate);
-
-	private:
 		void openPortFile	(const char* _port);
 		void setBaudRate	(struct termios& _serialPortInfo, unsigned _baudRate, const char* _port);
 		
 	private:
 		int		mFileDesc;
 	};
-
-	typedef SerialLinux SerialBase;
-
-}	// namespace dmc
-
-#endif // __linux__
-#endif // _DMCSERVER_PERIPHERALS_PLC_SERIALLINUX_H_
-
 
 	typedef SerialLinux SerialBase;
 
